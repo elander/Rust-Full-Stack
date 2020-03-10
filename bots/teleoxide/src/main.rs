1fc033f4b122572100b011adbd8c8a53fa0c0cd4
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-03-10 07:05:21 -0400

    Organize Rust subreddit latest posts finder before I commit.

--- bots/teleoxide/src/main.rs
+++ bots/teleoxide/src/main.rs
@@ -3,12 +3,15 @@ extern crate fstrings;
 
 // https://docs.rs/teloxide/0.2.0/teloxide/utils/markdown/fn.link.html
 use teloxide::prelude::*;
-// use teloxide::utils::markdown::link;
+
+// https://core.telegram.org/bots/api#markdownv2-style
+// It fails with MarkdownV2 so use this instead.
+// ERROR teloxide::error_handlers > Error: ApiError { status_code: 400, kind: Other }
+// I can't figure out what is wrong currently.
 use teloxide::types::ParseMode::Markdown;
+use teloxide::utils::markdown::link;
 
 use reqwest;
-// use std::collections::HashMap;
-
 use serde_json::Value;
 
 use console::Style;
@@ -16,6 +19,15 @@ use console::Style;
 #[tokio::main]
 async fn main() {
     run().await;
+}
+
+// https://users.rust-lang.org/t/fast-removing-chars-from-string/24554
+// "" is included in some JSON data so use this.
+fn remove_double_quote(text: String) -> String {
+    let double_quote = '"';
+    let empty_str = "";
+
+    text.replace(double_quote, empty_str)
 }
 
 async fn run() {
@@ -26,59 +38,64 @@ async fn run() {
 
     Dispatcher::new(bot)
         .messages_handler(|rx: DispatcherHandlerRx<Message>| {
-            rx.text_messages().for_each_concurrent(None, |(ctx, _raw_text_from_user)| async move {
-                let blue = Style::new()
-                    .blue();
+            rx.text_messages()
+                .for_each_concurrent(None, |(ctx, _raw_text_from_user)| async move {
+                    let blue = Style::new().blue();
 
-                // https://www.reddit.com/r/rust/new/.json?count=2
-                let subreddit = "rust";
-                let sort = "new";
-                let limit = "10"; // Max is 100, Use 2 here to ease the development.
+                    // Max is 100. Use 10 here to see the data better.
+                    // Refer to this. https://www.reddit.com/dev/api/
+                    // Visit it at the brwoser first.
+                    let end_point = "https://www.reddit.com/r/rust/new/.json?limit=10";
 
-                let complete_target = f!("https://www.reddit.com/r/{subreddit}/{sort}/.json?limit={limit}");
-                // println!("{}", &complete_target);
-                let body_text = reqwest::get(&complete_target).await.unwrap().text().await.unwrap();
+                    let body_text = reqwest::get(end_point).await.unwrap().text().await.unwrap();
 
-                // println!("{:#?}", &body_text);
+                    // Only extract what you want.
+                    // https://www.google.com/search?&q=extract+only+json+parts+I+want+serde
+                    let json_value: Value = serde_json::from_str(&body_text).unwrap();
 
-                // Only extract what you want.
-                // https://www.google.com/search?&q=extract+only+json+parts+I+want+serde
+                    // https://docs.serde.rs/serde_json/enum.Value.html#method.as_array
+                    // It becomes vector in Rust. Then, you can use its built in methods.
+                    let list_of_posts = json_value["data"]["children"].as_array().unwrap();
 
-                let json_value: Value = serde_json::from_str(&body_text).unwrap();
-                // println!("{:#?}", &json_value);
-                // let list_of_posts = &json_value["data"]["children"]; // array
-                // println!("List of posts\n {:#?}", &list_of_posts);
+                    let mut markdown = String::new();
 
-                // https://docs.serde.rs/serde_json/enum.Value.html#method.as_array
-                // It becomes vector in Rust. Then, you can use its built in methods.
-                let list_of_posts = json_value["data"]["children"].as_array().unwrap();
-                // println!("{:#?}", list_of_posts);
+                    // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.enumerate
+                    // for (pos, e) in v.iter().enumerate()
+                    for (index, post) in list_of_posts.iter().enumerate() {
+                        let title = &post["data"]["title"];
+                        let url = &post["data"]["url"];
 
-                let mut markdown = String::new();
-                let mut index: u8 = 1;
+                        let num = index + 1;
 
-                for post in list_of_posts {
-                    let title = &post["data"]["title"];
-                    let url = &post["data"]["url"];
+                        // let stdout_text = f!("{index}. {title}({blue.apply_to(&url)})"); // It doesn't work.
+                        let stdout_text = remove_double_quote(format!(
+                            "{}. {}({})",
+                            &num,
+                            &title,
+                            &blue.apply_to(&url)
+                        ));
+                        println!("{}", &stdout_text);
 
-                    let double_quote = '"';
-                    let empty_str = "";
+                        let post_link = link(url.as_str().unwrap(), title.as_str().unwrap());
 
-                    // https://users.rust-lang.org/t/fast-removing-chars-from-string/24554
-                    let complete_text = format!("{}. {}({})", &index, &title, &blue.apply_to(&url)).replace(double_quote, empty_str);
-                    // let complete_text = f!("{index}. {title}({blue.apply_to(&url)})"); // It doesn't work.
-                    println!("{}", &complete_text);
+                        // https://core.telegram.org/bots/api#markdownv2-style
+                        // It fails with MarkdownV2 so use this instead.
+                        // ERROR teloxide::error_handlers > Error: ApiError { status_code: 400, kind: Other }
+                        // I can't figure out what is wrong currently.
+                        let for_markdown = remove_double_quote(f!("{num}. {post_link}\n"));
+                        markdown.push_str(&for_markdown);
+                    }
 
-                    let for_markdown = f!("{index}. [{title}]({url})\n").replace(double_quote, empty_str);
-                    // let for_markdown = link(url.as_str().unwrap(), title.as_str().unwrap());
-                    markdown.push_str(&for_markdown);
-                    index = index + 1;
-                }
-
-                // https://docs.rs/teloxide/0.2.0/teloxide/dispatching/struct.DispatcherHandlerCx.html
-                // https://docs.rs/teloxide/0.2.0/teloxide/requests/struct.SendMessage.html
-                ctx.answer(markdown).parse_mode(Markdown).send().await.log_on_error().await;
-            })
+                    // https://docs.rs/teloxide/0.2.0/teloxide/dispatching/struct.DispatcherHandlerCx.html
+                    // https://docs.rs/teloxide/0.2.0/teloxide/requests/struct.SendMessage.html
+                    // Each method(parse_mode and send) returns SendMessage so you can bind them.
+                    ctx.answer(markdown)
+                        .parse_mode(Markdown)
+                        .send()
+                        .await
+                        .log_on_error()
+                        .await;
+                })
         })
         .dispatch()
         .await;
